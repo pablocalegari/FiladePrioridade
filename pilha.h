4fced55abb250bdf254322b1a8e08262e86568cb
pablocalegari <pabloantoniocalegari@gmail.com>
2026-05-04 20:26:41 -0300

    Definindo a Pilha

--- pilha.h
+++ pilha.h
@@ -1,1 +1,19 @@
+#ifndef PILHA_H
+#define PILHA_H
 
+#include "paciente.h"
+
+typedef struct No {
+    Paciente p;
+    struct No *prox;
+} No;
+
+typedef struct {
+    No *topo;
+} Pilha;
+
+void push(Pilha *p, Paciente paciente);
+Paciente pop(Pilha *p);
+int vazia(Pilha *p);
+
+#endif
