f0994c5d0f762bb4020877eaadfdda0ca3fcbede
pablocalegari <pabloantoniocalegari@gmail.com>
2026-05-04 20:31:52 -0300

    Definindo funções

--- utils.h
+++ utils.h
@@ -1,1 +1,16 @@
+#ifndef UTILS_H
+#define UTILS_H
 
+#include "fila.h"
+#include "pilha.h"
+
+//definindo a função de atender, tirar da fila e colocar pilha
+void atender(FilaPrioridade *fila, Pilha *pilha, int hora);
+
+//definindo a função de desfazer
+void desfazer(Pilha *pilha, FilaPrioridade *fila);
+
+
+void processarArquivo(char *nome, FilaPrioridade *fila, Pilha *pilha);
+
+#endif
