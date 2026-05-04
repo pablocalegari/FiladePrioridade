ca2b0c0e228e5c1a843475062dd697ddae202c58
pablocalegari <pabloantoniocalegari@gmail.com>
2026-05-04 20:22:31 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -1,1 +1,17 @@
+#ifndef FILA_H
+#define FILA_H
 
+#include "paciente.h"
+
+#define MAX 10000
+//definindo como vai ser a fila
+typedef struct {
+    Paciente dados[MAX];
+    int tamanho;
+} FilaPrioridade;
+
+void inserir(FilaPrioridade *f, Paciente p);
+Paciente remover(FilaPrioridade *f);
+void aumentarPrioridade(FilaPrioridade *f);
+
+#endif
