c393986318027c633012e57b948c9027576972b5
pablocalegari <pabloantoniocalegari@gmail.com>
2026-05-04 20:36:22 -0300

    Add patient queue management functions
    Implement functions to handle patient queue operations and process commands from a file.

--- utils.c
+++ utils.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "utils.h"
 
+void atender(FilaPrioridade *fila, Pilha *pilha, int hora) {
+    if (fila->tamanho == 0) return;
+//tira o paciente da fila
+    Paciente p = remover(fila);
+    p.horaAtendimento = hora;
+    push(pilha, p);
+}
+
+void desfazer(Pilha *pilha, FilaPrioridade *fila) {
+    if (vazia(pilha)) return;
+
+    Paciente p = pop(pilha);
+    p.prioridade = 999;
+    inserir(fila, p);
+}
+//abre o arquivo 
+void processarArquivo(char *nome, FilaPrioridade *fila, Pilha *pilha) {
+    FILE *arq = fopen(nome, "r");
+    char cmd[20];
+    int contador = 0;
+
+    while (fscanf(arq, "%s", cmd) != EOF) {
+
+        if (strcmp(cmd, "ADD") == 0) {
+            Paciente p;
+            fscanf(arq, "%d %s %d %d", &p.id, p.nome, &p.gravidade, &p.horaChegada);
+            p.prioridade = p.gravidade;
+
+            inserir(fila, p);
+            contador++;
+
+            if (contador % 5 == 0)
+                aumentarPrioridade(fila);
+        }
+
+        else if (strcmp(cmd, "ATENDER") == 0) {
+            atender(fila, pilha, rand() % 100);
+        }
+
+        else if (strcmp(cmd, "DESFAZER") == 0) {
+            desfazer(pilha, fila);
+        }
+    }
+
+    fclose(arq);
+}
