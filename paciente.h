2768f55c14641d231ab76dbb66c3f7111acd2f6f
pablocalegari <pabloantoniocalegari@gmail.com>
2026-05-04 20:20:11 -0300

    Arquivo para definir o que cada Paciente vai ter de caracteristicas

--- paciente.h
+++ paciente.h
@@ -1,1 +1,13 @@
+#ifndef PACIENTE_H
+#define PACIENTE_H
 
+typedef struct {
+    int id;
+    char nome[100];
+    int gravidade;
+    int prioridade;
+    int horaChegada;
+    int horaAtendimento;
+} Paciente;
+
+#endif
