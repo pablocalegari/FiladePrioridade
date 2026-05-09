0076072813eee6f2c18042190113e16b2c91d65b
pablocalegari <pabloantoniocalegari@gmail.com>
2026-05-08 23:42:28 -0300

    Refactor atendimento and desfazer functions

--- utils.c
+++ utils.c
@@ -5,7 +5,7 @@
 
 void atender(FilaPrioridade *fila, Pilha *pilha, int hora) {
     if (fila->tamanho == 0) return;
-//tira o paciente da fila
+
     Paciente p = remover(fila);
     p.horaAtendimento = hora;
     push(pilha, p);
@@ -15,12 +15,19 @@ void desfazer(Pilha *pilha, FilaPrioridade *fila) {
     if (vazia(pilha)) return;
 
     Paciente p = pop(pilha);
-    p.prioridade = 999;
+    p.prioridade = 6; // prioridade máxima ao desfazer
     inserir(fila, p);
 }
-//abre o arquivo 
+
 void processarArquivo(char *nome, FilaPrioridade *fila, Pilha *pilha) {
     FILE *arq = fopen(nome, "r");
+
+   
+    if (!arq) {
+        printf("Erro: arquivo '%s' nao encontrado ou sem permissao de leitura.\n", nome);
+        return;
+    }
+
     char cmd[20];
     int contador = 0;
 
@@ -39,7 +46,8 @@ void processarArquivo(char *nome, FilaPrioridade *fila, Pilha *pilha) {
         }
 
         else if (strcmp(cmd, "ATENDER") == 0) {
-            atender(fila, pilha, rand() % 100);
+            contador++;
+            atender(fila, pilha, contador);
         }
 
         else if (strcmp(cmd, "DESFAZER") == 0) {
